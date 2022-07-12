c6d2c84a50411cd65a6b403b82ec6228d35f9c86
yamoo9 <yamoo9@naver.com>
2022-07-12 15:53:10 +0900

    learn. Conditional Operators
    - optional chaining
    - nullish coalescing operator

--- src/chapter/10-conditional-operators.js
+++ src/chapter/10-conditional-operators.js
@@ -1,28 +1,34 @@
 /* eslint-disable jest/expect-expect */
 /* eslint-disable no-unused-vars */
 
-/* 3항 연산 식(터너리), null 병합 연산자, 옵셔널 체이닝을 활용해 코드를 작성합니다. ---------------- */
+// React JSX 식 (JS extenstions like XML)
+// 값을 반환하는 것: 식, 연산자, 함수 등등
+// 값을 반환하지 않는 것: if, while, for, switch 문 ...
+
+// 연산자
+// 피연산자
+// 단항
+// 이항
+// 삼항
+
+/* 3항 연산 식(터너리, ternary), nullish 병합 연산자, 옵셔널 체이닝을 활용해 코드를 작성합니다. ---------------- */
 
 // ----------------------------------------------------------------------------------
 // Nullish coalescing operator
 // ----------------------------------------------------------------------------------
 
 const defaultArg = (value, initialValue) => {
-  // Ternaries
-  // 아래 코드를 3항 연산 식으로 변경합니다.
-  if (value === null || value === undefined) {
-    return initialValue;
-  } else {
-    return value;
-  }
+  // return (value === null || value === undefined) ? initialValue : value;
 
   // Nullish coalescing operator
   // null 병합 연산자를 사용한 식으로 변경합니다.
+  // 0, '', NaN
+  return value ?? initialValue;
 };
 
-const ellipsisText = (text, limit) => {
+const ellipsisText = (text, limit = 100) => {
   // 기본 매개변수를 활용해 코드를 변경합니다.
-  limit = defaultArg(limit, 100);
+  // limit = defaultArg(limit, 100);
   return `${text.slice(0, limit).trim()}...`;
 };
 
@@ -42,28 +48,37 @@ const company = Object.freeze({
     lng: -39,
   },
   getLocation() {
-    return Object.values(company.location);
+    const [lat, lng] = Object.values(company.location); // [321, -39]
+    return { lat, lng };
   },
 });
 
-let companyName, companyLat;
+let companyName, companyLat; // undefined
 
-if ('companyName' in company) {
-  companyName = company.companyName;
-}
+// if ('companyName' in company) {
+//   companyName = company.companyName;
+// }
 
-console.log('companyName = ', companyName);
+companyName = company?.companyName;
 
-if ('location' in company) {
-  if ('lat' in company.location) companyLat = company.location.lat;
-}
+console.log('companyName = ', companyName); // undefined
+
+// if ('location' in company) {
+//   if ('lat' in company.location) companyLat = company.location.lat; // 321
+// }
+
+companyLat = company.location?.lat;
 
 console.log('companyLat = ', companyLat);
 
-if ('getFoundingDate' in company) {
-  if (typeof company.getFoundingDate === 'function') company.getFoundingDate();
-}
+// if ('getFoundingDate' in company) {
+//   if (typeof company.getFoundingDate === 'function') company.getFoundingDate();
+// }
 
-if ('getLocation' in company) {
-  if (typeof company.getLocation === 'function') company.getLocation();
-}
+company.getFoundingDate?.(); // ERROR
+
+// if ('getLocation' in company) {
+//   if (typeof company.getLocation === 'function') company.getLocation();
+// }
+
+console.log(company.getLocation?.());
