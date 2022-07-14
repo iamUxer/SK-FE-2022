34fe46fc7bb4790de99706600a46b90f05a6ff7a
yamoo9 <yamoo9@naver.com>
2022-07-14 15:19:48 +0900

    learn. 객체 리스트 렌더링 (Object.entites 활용)

--- random-count-up/src/pages/JsxInAction.js
+++ random-count-up/src/pages/JsxInAction.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 
+import React from 'react';
 import { EmojiOops, PrettyPrintCode } from 'components';
 // import db from 'api/db.json';
 
@@ -99,12 +100,28 @@ const ListRedering = () => {
 
       {/* 객체 리스트 렌더링 */}
       <dl className="descriptionList">
+        {/* <dt>설명 용어</dt>
+        <dd>설명 내용</dd> */}
         {/* 웹 표준을 준수해 설명 목록을 화면에 출력해봅니다. */}
         {/* `db` 객체의 "속성", "값" 쌍을 순환하여 리스트 렌더링 해보세요. */}
         {/* 값의 유형이 객체 또는 배열인 경우, 코드가 화면에 출력되도록 설정합니다. */}
         {/* `isArray`, `isObject` 유틸리티 함수를 만들어 활용하세요. */}
         {/* 코드를 화면에 출력할 때는 <PrettyPrintCode /> 컴포넌트를 활용합니다. (`code` 속성) */}
-        <PrettyPrintCode />
+        {/* <PrettyPrintCode code={db} /> */}
+        {Object.entries(db).map(([key, value]) => {
+          return (
+            <React.Fragment key={key}>
+              <dt>{key}</dt>
+              <dd>
+                {typeof value === 'object' ? (
+                  <PrettyPrintCode code={value} />
+                ) : (
+                  value
+                )}
+              </dd>
+            </React.Fragment>
+          );
+        })}
       </dl>
     </div>
   );
