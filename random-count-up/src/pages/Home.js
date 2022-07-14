e7efbebaa69576776eb7d698d2adea30380f39f2
yamoo9 <yamoo9@naver.com>
2022-07-14 14:55:15 +0900

    learn. React 조건부, 리스트 렌더링

--- random-count-up/src/pages/Home.js
+++ random-count-up/src/pages/Home.js
@@ -3,21 +3,24 @@ import { AppLink } from 'components';
 import { Link } from 'react-router-dom';
 
 export default function HomePage({ linkText }) {
-  return [
-    <ReactLogo className="App-logo" title="React" />,
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>,
-    <AppLink href="https://reactjs.org" external>
-      {linkText}
-    </AppLink>,
-    <p>
-      <Link to="/products">Products</Link>
-    </p>,
-    <p>
-      <Link to="/jsx-in-action">
-        <abbr title="JavaScript extention like XML Syntax">JSX</abbr> in Action
-      </Link>
-    </p>,
-  ];
+  return (
+    <>
+      <ReactLogo className="App-logo" title="React" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <AppLink href="https://reactjs.org" external>
+        {linkText}
+      </AppLink>
+      <p>
+        <Link to="/products">Products</Link>
+      </p>
+      <p>
+        <Link to="/jsx-in-action">
+          <abbr title="JavaScript extention like XML Syntax">JSX</abbr> in
+          Action
+        </Link>
+      </p>
+    </>
+  );
 }
--- random-count-up/src/pages/JsxInAction.js
+++ random-count-up/src/pages/JsxInAction.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 
 import { EmojiOops, PrettyPrintCode } from 'components';
+// import db from 'api/db.json';
 
 export default function JsxInActionPage() {
   return (
     <div className="jsxInAction">
-      <ConditionalRendering />
+      <ListRedering />
     </div>
   );
 }
@@ -15,37 +16,71 @@ export default function JsxInActionPage() {
 let error = null;
 
 // 오류 메시지를 포함한 객체를 추가해보세요.
+error = {
+  message: '네트워크 오류가 발생했습니다.',
+  print() {
+    console.error(this.message);
+    return this.message;
+  },
+};
 
 /* -------------------------------------------------------------------------- */
 
 const ConditionalRendering = () => {
-  if (error === null || error === undefined) {
-    console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
-  }
+  // if (error === null || error === undefined) {
+  //   console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
+  // }
 
-  error && typeof error.log === 'function' && error.log();
+  // nullish 병합 연산자
+  error ?? console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
 
-  // JSX 조건부 렌더링: if 문
-  // 오류가 존재하면 렌더링 되도록 코드를 작성합니다.
-  const displayErrorState = (
-    <div className="container">
-      <h1 className="headline">
-        <EmojiOops height={200} />
-        {/* 오류 메시지가 출력되도록 코드를 변경합니다. */}
-        {error}
-      </h1>
-    </div>
-  );
+  // 옵셔널 체이닝
+  // error && typeof error.log === 'function' && error.log();
+  error.log?.();
 
   return (
     <div className="container">
-      <h1 className="headline">React 조건부 렌더링</h1>
-      <p>오류가 존재하면 렌더링 되도록 코드를 작성합니다.</p>
+      <h1 className="headline">
+        {error ? (
+          // ? [<EmojiOops key="emoji oops" height={200} />, error.message]
+          <>
+            <EmojiOops height={200} />
+            {error.message}
+          </>
+        ) : (
+          'React 조건부 렌더링'
+        )}
+      </h1>
+      {/* {!error ? <p>오류가 존재하면 렌더링 되도록 코드를 작성합니다.</p> : null} */}
+      {/* 논리곱 (||) */}
+      {/* {error || <p>오류가 존재하면 렌더링 되도록 코드를 작성합니다.</p>} */}
+      {/* 논리합 (&&) */}
+      {!error && <p>오류가 존재하면 렌더링 되도록 코드를 작성합니다.</p>}
     </div>
   );
 };
 
 /* -------------------------------------------------------------------------- */
+
+// CommonJS 진영의 모듈 로드 방식 사용
+// CRA (webpack, CommonJS 모듈 활용)
+
+const db = require('api/db.json');
+const {
+  navigation: { items },
+} = db;
+
+const ListItem = ({ link, text }) => (
+  <li>
+    <a href={link}>{text}</a>
+  </li>
+);
+
+const renderList = (list) => {
+  return list.map(({ link, text }) => (
+    <ListItem key={link} link={link} text={text} />
+  ));
+};
 
 // `api/db.json` 데이터에서 `navigation.items` 데이터를 화면에 출력해보세요.
 // `list` 매개 변수를 순환해 아이템 리스트를 반환하는 `renderList` 함수를 만들어 활용해봅니다.
@@ -59,9 +94,7 @@ const ListRedering = () => {
       <nav className="globalNavigation">
         {/* 웹 표준을 준수해 비순차 목록을 화면에 출력해봅니다. */}
         {/* renderList(list) 함수를 실행해 리스트 렌더링 처리해봅니다. */}
-        <ul>
-          <li>리스트를 렌더링 해보세요.</li>
-        </ul>
+        <ul>{renderList(items)}</ul>
       </nav>
 
       {/* 객체 리스트 렌더링 */}
