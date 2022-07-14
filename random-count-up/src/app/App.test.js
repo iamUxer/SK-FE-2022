0e9ac41d369fca83bbe993e7e3be2eefb7ade48d
yamoo9 <yamoo9@naver.com>
2022-07-14 09:30:48 +0900

    learn. React 컴포넌트 테스트 (App)

--- random-count-up/src/app/App.test.js
+++ random-count-up/src/app/App.test.js
@@ -1,9 +1,15 @@
-/* eslint-disable testing-library/no-debugging-utils */
-import App from './App';
 import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test('"learn react" 텍스트를 포함하는 요소가 문서에 렌더링 되었는가?', () => {
+test('"React 학습하기" 텍스트를 포함하는 요소가 문서에 렌더링 되었는가?', () => {
   render(<App />);
-  const learnLink = screen.getByText(/learn react/i);
+  const learnLink = screen.getByText(/react 학습하기/i);
   expect(learnLink).toBeInTheDocument();
+});
+
+test('React 로고 이미지의 대체 텍스트는 "React" 이다.', () => {
+  render(<App />);
+  const logoImage = screen.getByRole('img');
+  expect(logoImage).toHaveAttribute('alt');
+  expect(logoImage.getAttribute('alt')).toBe('React');
 });
