9685426b1fce0622dbb8a077cfe15776bdc30ea2
yamoo9 <yamoo9@naver.com>
2022-07-15 10:11:41 +0900

    learn. React Hooks - useState

--- client/src/pages/LearnHooks/index.jsx
+++ client/src/pages/LearnHooks/index.jsx
@@ -1,10 +1,29 @@
 import styles from './style.module.css';
+import { useState } from 'react';
 
-const LearnHooksPage = () => (
-  <div id="learnClass" className={styles.container} lang="en">
-    <h1>React</h1>
-    <button type="button">Toggle Subject</button>
-  </div>
-);
+// 2019 React Hooks
+// state hook (use*)
+
+const LearnHooksPage = () => {
+  const [state, setState] = useState({
+    subject: 'React',
+  });
+
+  const handleToggleSubject = () => {
+    let subject = state.subject.includes('React') ? 'Vue' : 'React';
+    setState({ subject });
+
+    console.log('업데이트 된 함수 컴포넌트의 상태 = ', state.subject);
+  };
+
+  return (
+    <div id="learnClass" className={styles.container} lang="en">
+      <h1>{state.subject}</h1>
+      <button type="button" onClick={handleToggleSubject}>
+        Toggle Subject
+      </button>
+    </div>
+  );
+};
 
 export default LearnHooksPage;
